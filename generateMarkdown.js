0f595afba7f9994a5185c2c461c866f7f109e177
Deboh12 <deborah.alexander12@yahoo.com>
2023-12-28 13:05:56 -0600

    wrote basic markdown README information on generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -14,6 +14,38 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  For any questions, please contact me:
+  - GitHub: [${data.username}](https://github.com/${data.username})
+  - Email: ${data.email}
 `;
 }
 
